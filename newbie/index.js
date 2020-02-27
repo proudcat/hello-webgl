098d596f261c30e4b185f30e4961198ff8104a6e
yulijun <koalaylj@gmail.com>
2020-02-27 23:20:36 +0800

    删除无用代码

--- newbie/index.js
+++ newbie/index.js
@@ -1,18 +1,11 @@
 
-// import { Loader } from 'resource-loader'
-// import * as dat from 'dat.gui'
 import * as examples from './js'
 
 // import {Spector,EmbeddedFrontend} from 'spectorjs'
-    
-// const gui = new dat.GUI()
-// gui.useLocalStorage = false
 
 // class Render{
 //   constructor(){
 
-//     // this.canvas = document.querySelector('#stage')
-//     // this.context = this.canvas.getContext('webgl')
 //     // this.spector = new Spector()
 
 //     // this.spector.onCapture.add(result => {
@@ -20,11 +13,6 @@ import * as examples from './js'
 //     //   resultView.display()
 //     //   resultView.addCapture(result)
 //     // })
-   
-//     // if (!this.context) {
-//     //   console.log('Failed to get the rendering context for WebGL')
-//     //   return
-//     // }
 //   }
 
 //   capture(){
@@ -45,14 +33,9 @@ import * as examples from './js'
 // }
 
 function setup(){
-
-  // gui.add({name:'webgl examples'},'name')
-  // gui.add({capture:()=>render.capture()},'capture')
-
   for (let [name, Demo] of Object.entries(examples)) {
     new Demo(name)
   }
-
 }
 
 window.onload = setup
