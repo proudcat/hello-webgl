d685d6db027703d14d3629be71bc6fd99357e14e
yulijun <koalaylj@gmail.com>
2020-02-29 00:41:38 +0800

    ch04 01-model-matrix

--- webgl-programming-guide/ch04/01-model-matrix.js
+++ webgl-programming-guide/ch04/01-model-matrix.js
@@ -1,23 +1,32 @@
 import Demo from '../common/demo'
-import vert from '../shaders/a_pos_u_trans.vs'
+import Matrix4 from '../common/matrix4.js'
 import frag from '../shaders/red.fs'
+import vert from '../shaders/a_pos_u_model.vs'
 
-export class TranslationRectangle extends Demo{
+export class ModelMatrix extends Demo{
 
   constructor(name){
     super(name,{vert,frag})
 
     let gl = this.ctx
     
-    let vertices = new Float32Array([-0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5])
-
+    let vertices = new Float32Array([
+      0.0, 0.3, -0.3, -0.3, 0.3, -0.3
+    ])
     //定点个数
-    this.count = 4
+    this.count = 3
 
-    let tx = 0.5,ty=0.5,tz=0.0
+    let angle = 45
+    let tx = 0.5
 
-    let u_Translation = gl.getUniformLocation(gl.program, 'u_Translation')
-    gl.uniform4f(u_Translation, tx, ty, tz, 0.0)
+    let modelMatrix = new Matrix4()
+    modelMatrix.setRotate(angle, 0, 0, 1)
+    modelMatrix.translate(tx, 0, 0)
+
+    let u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+    
+    //注意 WebGL没有实现矩阵转置，所以第二个参数必须是false。
+    gl.uniformMatrix4fv(u_ModelMatrix,false, modelMatrix.elements)
     
     // Create a buffer object
     let vertexBuffer = gl.createBuffer()
@@ -39,16 +48,13 @@ export class TranslationRectangle extends Demo{
     this.render()
   }
   render() {
+
     let gl = this.ctx
     
     gl.clearColor(0.0, 0.0, 0.0, 1.0)
     gl.clear(gl.COLOR_BUFFER_BIT)
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count)
-    // gl.drawArrays(gl.TRIANGLE_FAN, 0, this.count)
-    // gl.drawArrays(gl.LINES, 0, this.count)
-    // gl.drawArrays(gl.LINE_STRIP, 0, this.count)
-    // gl.drawArrays(gl.LINE_LOOP, 0, this.count)
+    gl.drawArrays(gl.TRIANGLES, 0, this.count)
   }
 
 }
--- webgl-programming-guide/ch04/index.js
+++ webgl-programming-guide/ch04/index.js
@@ -1,1 +1,2 @@
 export * from './00-rotate-triangle-matrix4'
+export * from './01-model-matrix'
