6f1a12b82d2b8c3df0bfde0f167ed1e38feca986
yulijun <koalaylj@gmail.com>
2020-02-29 03:00:56 +0800

    ch04 rotation animation

---
+++ webgl-programming-guide/ch04/02-rotate-triangle-animation.js
@@ -0,0 +1,71 @@
+import Demo from '../common/demo'
+import Matrix4 from '../common/matrix4.js'
+import frag from '../shaders/red.fs'
+import vert from '../shaders/a_pos_u_model.vs'
+
+
+const ANGLE_STEP = 45.0
+
+
+export class RotateTriangleAnimation extends Demo{
+
+  constructor(name){
+    super(name,{vert,frag})
+
+    let gl = this.ctx
+    
+    let vertices = new Float32Array([
+      0.0, 0.3, -0.3, -0.3, 0.3, -0.3
+    ])
+
+    //顶点个数
+    this.count = 3
+    this.currentAngle = 0.0 
+    this.lastTime = Date.now()
+
+    let vertexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+    let a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(a_Position)
+    
+    this.modelMatrix = new Matrix4()
+    this.u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    this.update()
+  }
+
+  animate() {
+    let now = Date.now()
+    let elapsed = now - this.lastTime
+    this.lastTime = now
+
+    // Update the current rotation angle (adjusted by the elapsed time)
+    let newAngle = this.currentAngle + (ANGLE_STEP * elapsed) / 1000.0
+
+    this.currentAngle = newAngle %360
+  }
+
+  update(){
+    this.animate()
+    this.render()
+    requestAnimationFrame(()=>this.update())
+  }
+
+  render() {
+    let gl = this.ctx
+    
+    this.modelMatrix.setRotate(this.currentAngle, 0, 0, 1)
+    this.modelMatrix.translate(0.35, 0, 0)
+ 
+    gl.uniformMatrix4fv(this.u_ModelMatrix, false, this.modelMatrix.elements)
+
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    gl.drawArrays(gl.TRIANGLES, 0, this.count)
+  }
+
+}
--- webgl-programming-guide/ch04/index.js
+++ webgl-programming-guide/ch04/index.js
@@ -1,2 +1,3 @@
 export * from './00-rotate-triangle-matrix4'
 export * from './01-model-matrix'
+export * from './02-rotate-triangle-animation'
