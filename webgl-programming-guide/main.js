89ec15f4ca82b6db68668b5cd6245b05dbd518c3
yulijun <koalaylj@gmail.com>
2020-02-29 00:40:27 +0800

    fix

--- webgl-programming-guide/main.js
+++ webgl-programming-guide/main.js
@@ -10,8 +10,8 @@ let chapters = {
   ch02, ch03, ch04
 }
 
-//上一个显示的章节
-let last = {
+//当前章节
+let current = {
   chapter:''
 }
 
@@ -24,25 +24,29 @@ let demoList = []
  */
 function show(chapter='ch04'){
 
-  if(last.chapter == chapter){
+  if(current.chapter == chapter){
     return
   }else{
     demoList.forEach(demo => demo.destroy())
     demoList=[]
   }
+
+  // if(!chapter){
+  //   chapter = current.chapter
+  // }
   
   for (let [name, Demo] of Object.entries(chapters[chapter])) {
     let demo = new Demo(name)
     demoList.push(demo)
   }
 
-  last.chapter = chapter
+  current.chapter = chapter
 }
 
 function setup(){
 
   gui.close()
-  gui.add(last, 'chapter').listen()
+  gui.add(current, 'chapter').listen()
 
   for (const chapter in chapters) {
     gui.add({select: () => show(chapter)},'select').name(chapter)
