261ed5c4ec9544132e6022d27d18fa6115799724
yulijun <koalaylj@gmail.com>
2020-03-01 03:21:20 +0800

    view *  model

---
+++ webgl-programming-guide/ch07/01-look-at-rotated-triangles.js
@@ -0,0 +1,68 @@
+import Demo from '../common/demo'
+import Matrix4 from '../common/matrix4.js'
+import vert from '../shaders/a_pos_av_color_u_view_u_model.vs'
+import frag from '../shaders/v_color.fs'
+
+export class LookAtRotatedriangle extends Demo {
+
+  constructor(name) {
+    super(name, { vert, frag })
+
+    let gl = this.ctx
+
+    let vertices = new Float32Array([
+      // Vertex coordinates and color(RGBA)
+      0.0, 0.5, -0.4, 0.4, 1.0, 0.4, // The back green one
+      -0.5, -0.5, -0.4, 0.4, 1.0, 0.4,
+      0.5, -0.5, -0.4, 1.0, 0.4, 0.4,
+
+      0.5, 0.4, -0.2, 1.0, 0.4, 0.4, // The middle yellow one
+      -0.5, 0.4, -0.2, 1.0, 1.0, 0.4,
+      0.0, -0.6, -0.2, 1.0, 1.0, 0.4,
+
+      0.0, 0.5, 0.0, 0.4, 0.4, 1.0,  // The front blue one 
+      -0.5, -0.5, 0.0, 0.4, 0.4, 1.0,
+      0.5, -0.5, 0.0, 1.0, 0.4, 0.4,
+    ])
+
+    //顶点个数
+    this.count = 9
+
+    let viewMatrix = new Matrix4()
+    viewMatrix.setLookAt(0.20, 0.25, 0.25, 0, 0, 0, 0, 1, 0)
+
+    let u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix')
+    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements)
+
+    let modelMatrix = new Matrix4()
+    modelMatrix.setRotate(-30,0,0,1)
+    let u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
+
+    let vertexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+    let FSIZE = vertices.BYTES_PER_ELEMENT
+
+    let a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0)
+    gl.enableVertexAttribArray(a_Position)
+
+    let a_Color = gl.getAttribLocation(gl.program, 'a_Color')
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3)
+    gl.enableVertexAttribArray(a_Color)
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    this.render()
+  }
+
+  render() {
+    let gl = this.ctx
+
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.drawArrays(gl.TRIANGLES, 0, this.count)
+  }
+
+}
--- webgl-programming-guide/ch07/index.js
+++ webgl-programming-guide/ch07/index.js
@@ -1,1 +1,2 @@
 export * from './00-look-at-triangles'
+export * from './01-look-at-rotated-triangles'
