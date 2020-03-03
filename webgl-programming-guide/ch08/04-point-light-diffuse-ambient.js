a94b492a85e991eac6cf078838b71681034a9f7c
yulijun <koalaylj@gmail.com>
2020-03-03 08:05:21 +0800

    点光源

---
+++ webgl-programming-guide/ch08/04-point-light-diffuse-ambient.js
@@ -0,0 +1,129 @@
+import Demo from '../common/demo'
+import frag from '../shaders/v_color.frag'
+import Matrix4 from '../common/matrix4.js'
+import vert from '../shaders/point-light-diffuse-ambient-normal.vert'
+
+export class PointLightDiffuseAmbient extends Demo {
+
+  constructor(name) {
+    super(name, { vert, frag })
+
+    let gl = this.ctx
+
+    this.initVertexBuffer()
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.enable(gl.DEPTH_TEST)
+
+    let u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+    let u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix')
+    let u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix')
+    let u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor')
+    let u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition')
+    let u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight')
+
+    gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0)
+
+    // Set the light direction (in the world coordinate)
+    gl.uniform3f(u_LightPosition, 2.3, 4.0, 3.5)
+    gl.uniform3f(u_AmbientLight, 0.2, 0.2, 0.2)
+
+    let modelMatrix = new Matrix4()
+    let mvpMatrix = new Matrix4()
+    let normalMatrix = new Matrix4()
+
+    modelMatrix.setRotate(90, 0, 1, 0)
+    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
+
+    mvpMatrix.setPerspective(30, this.$canvas.width / this.$canvas.height, 1, 100)
+    mvpMatrix.lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0)
+    mvpMatrix.multiply(modelMatrix)
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements)
+
+    normalMatrix.setInverseOf(modelMatrix)
+    normalMatrix.transpose()
+    gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements)
+
+    this.render()
+  }
+
+  initVertexBuffer() {
+
+    let gl = this.ctx
+
+    // Create a cube
+    //    v6----- v5
+    //   /|      /|
+    //  v1------v0|
+    //  | |     | |
+    //  | |v7---|-|v4
+    //  |/      |/
+    //  v2------v3
+    var vertices = new Float32Array([
+      2.0, 2.0, 2.0, -2.0, 2.0, 2.0, -2.0, -2.0, 2.0, 2.0, -2.0, 2.0, // v0-v1-v2-v3 front
+      2.0, 2.0, 2.0, 2.0, -2.0, 2.0, 2.0, -2.0, -2.0, 2.0, 2.0, -2.0, // v0-v3-v4-v5 right
+      2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0, 2.0, -2.0, -2.0, 2.0, 2.0, // v0-v5-v6-v1 up
+      -2.0, 2.0, 2.0, -2.0, 2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, 2.0, // v1-v6-v7-v2 left
+      -2.0, -2.0, -2.0, 2.0, -2.0, -2.0, 2.0, -2.0, 2.0, -2.0, -2.0, 2.0, // v7-v4-v3-v2 down
+      2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, 2.0, -2.0, 2.0, 2.0, -2.0  // v4-v7-v6-v5 back
+    ])
+
+    // Colors
+    var colors = new Float32Array([
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v1-v2-v3 front
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v3-v4-v5 right
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v5-v6-v1 up
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v1-v6-v7-v2 left
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v7-v4-v3-v2 down
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0      // v4-v7-v6-v5 back
+    ])
+
+    var indices = new Uint8Array([
+      // Indices of the vertices
+      0,  1,  2,  0,  2,  3,    // front
+      4,  5,  6,  4,  6,  7,    // right
+      8,  9,  10, 8,  10, 11,   // up
+      12, 13, 14, 12, 14, 15,   // left
+      16, 17, 18, 16, 18, 19,   // down
+      20, 21, 22, 20, 22, 23    // back
+    ])
+
+    let normals = new Float32Array([    // Normal
+      0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  // v0-v1-v2-v3 front
+      1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  // v0-v3-v4-v5 right
+      0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  // v0-v5-v6-v1 up
+      -1.0, 0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0,  // v1-v6-v7-v2 left
+      0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  // v7-v4-v3-v2 down
+      0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0   // v4-v7-v6-v5 back
+    ])
+
+    //顶点个数
+    this.count = indices.length
+
+    let indexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
+    this.initArrayBuffer('a_Position', vertices, 3)
+    this.initArrayBuffer('a_Color', colors, 3)
+    this.initArrayBuffer('a_Normal', normals, 3)
+
+  }
+
+  initArrayBuffer(attribute, data, num) {
+    let gl = this.ctx
+    let buffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
+    let location = gl.getAttribLocation(gl.program, attribute)
+    gl.vertexAttribPointer(location, num, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(location)
+  }
+
+  render() {
+    let gl = this.ctx
+    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT)
+    gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_BYTE, 0)
+  }
+
+}
---
+++ webgl-programming-guide/ch08/05-point-light-diffuse-ambient-animation.js
@@ -0,0 +1,148 @@
+import Demo from '../common/demo'
+import frag from '../shaders/v_color.frag'
+import Matrix4 from '../common/matrix4.js'
+import vert from '../shaders/point-light-diffuse-ambient-normal.vert'
+
+export class PointLightDiffuseAmbientAnimation extends Demo {
+
+  constructor(name) {
+    super(name, { vert, frag })
+
+    let gl = this.ctx
+
+    this.initVertexBuffer()
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+    gl.enable(gl.DEPTH_TEST)
+
+    this.u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+    this.u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix')
+    this.u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix')
+    let u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor')
+    let u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition')
+    let u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight')
+
+    this.vpMatrix = new Matrix4()
+    this.vpMatrix.setPerspective(30, this.$canvas.width / this.$canvas.height, 1, 100)
+    this.vpMatrix.lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0)
+
+    gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0)
+    gl.uniform3f(u_LightPosition, 2.3, 4.0, 3.5)
+    gl.uniform3f(u_AmbientLight, 0.2, 0.2, 0.2)
+
+    this.angle = 0.0
+
+    this.modelMatrix = new Matrix4()
+    this.mvpMatrix = new Matrix4()
+    this.normalMatrix = new Matrix4()
+
+    this.lastNow = Date.now()
+
+    this.update()
+  }
+
+  update() {
+    let gl = this.ctx
+    let now = Date.now()
+    let elapsed = now - this.lastNow
+    this.lastNow = now
+    this.angle = this.angle + 30.0 * elapsed / 1000.0
+    this.angle %= 360
+
+    this.modelMatrix.setRotate(this.angle, 0, 1, 0) // Rotate around the y-axis
+    gl.uniformMatrix4fv(this.u_ModelMatrix, false, this.modelMatrix.elements)
+
+    this.mvpMatrix.set(this.vpMatrix).multiply(this.modelMatrix)
+    gl.uniformMatrix4fv(this.u_MvpMatrix, false, this.mvpMatrix.elements)
+
+    this.normalMatrix.setInverseOf(this.modelMatrix)
+    this.normalMatrix.transpose()
+    gl.uniformMatrix4fv(this.u_NormalMatrix, false, this.normalMatrix.elements)
+
+    this.render()
+
+    requestAnimationFrame(() => this.update())
+  }
+
+  initVertexBuffer() {
+
+    let gl = this.ctx
+
+    // Create a cube
+    //    v6----- v5
+    //   /|      /|
+    //  v1------v0|
+    //  | |     | |
+    //  | |v7---|-|v4
+    //  |/      |/
+    //  v2------v3
+    var vertices = new Float32Array([
+      2.0, 2.0, 2.0, -2.0, 2.0, 2.0, -2.0, -2.0, 2.0, 2.0, -2.0, 2.0, // v0-v1-v2-v3 front
+      2.0, 2.0, 2.0, 2.0, -2.0, 2.0, 2.0, -2.0, -2.0, 2.0, 2.0, -2.0, // v0-v3-v4-v5 right
+      2.0, 2.0, 2.0, 2.0, 2.0, -2.0, -2.0, 2.0, -2.0, -2.0, 2.0, 2.0, // v0-v5-v6-v1 up
+      -2.0, 2.0, 2.0, -2.0, 2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, 2.0, // v1-v6-v7-v2 left
+      -2.0, -2.0, -2.0, 2.0, -2.0, -2.0, 2.0, -2.0, 2.0, -2.0, -2.0, 2.0, // v7-v4-v3-v2 down
+      2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, 2.0, -2.0, 2.0, 2.0, -2.0  // v4-v7-v6-v5 back
+    ])
+
+    // Colors
+    var colors = new Float32Array([
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v1-v2-v3 front
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v3-v4-v5 right
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v0-v5-v6-v1 up
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v1-v6-v7-v2 left
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,     // v7-v4-v3-v2 down
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0      // v4-v7-v6-v5 back
+    ])
+
+    var indices = new Uint8Array([
+      // Indices of the vertices
+      0,  1,  2,  0,  2,  3,    // front
+      4,  5,  6,  4,  6,  7,    // right
+      8,  9,  10, 8,  10, 11,   // up
+      12, 13, 14, 12, 14, 15,   // left
+      16, 17, 18, 16, 18, 19,   // down
+      20, 21, 22, 20, 22, 23    // back
+    ])
+
+    let normals = new Float32Array([    // Normal
+      0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  // v0-v1-v2-v3 front
+      1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  // v0-v3-v4-v5 right
+      0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  0.0,  1.0,  0.0,  // v0-v5-v6-v1 up
+      -1.0, 0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0,  // v1-v6-v7-v2 left
+      0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  // v7-v4-v3-v2 down
+      0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0,  0.0,  0.0, -1.0   // v4-v7-v6-v5 back
+    ])
+
+    //顶点个数
+    this.count = indices.length
+
+    this.initArrayBuffer('a_Position', vertices, 3)
+    this.initArrayBuffer('a_Color', colors, 3)
+    this.initArrayBuffer('a_Normal', normals, 3)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+    let indexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
+  }
+
+  initArrayBuffer(attribute, data, num) {
+    let gl = this.ctx
+    let buffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW)
+    let location = gl.getAttribLocation(gl.program, attribute)
+    gl.vertexAttribPointer(location, num, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(location)
+  }
+
+  render() {
+    let gl = this.ctx
+    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT)
+    gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_BYTE, 0)
+  }
+
+}
